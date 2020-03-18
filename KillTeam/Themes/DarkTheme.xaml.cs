26aa969c9e418d75e36c1c1ddff1e1f5dce341c6
Thomas L <lacombe.thomas@outlook.com>
2020-03-18 12:47:26 +0100

    DarkTheme and LightTheme creation

---
+++ KillTeam/Themes/DarkTheme.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace KillTeam.Themes
+{
+    public partial class DarkTheme : ResourceDictionary
+    {
+        public DarkTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}

62c7efb1d967aa45ed45f084725b07e0d619c2dd
Thomas L <lacombe.thomas@outlook.com>
2020-03-18 16:35:40 +0100

    Hard coded translation of the data loss rollback Alert

--- KillTeam/Views/DatabaseLoadPage.xaml.cs
+++ KillTeam/Views/DatabaseLoadPage.xaml.cs
@@ -3,6 +3,7 @@ using KillTeam.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -31,12 +32,23 @@ namespace KillTeam.Views
             {
                 if (showModal)
                 {
-                    bool result = await Application.Current.MainPage.DisplayAlert(
-                        "Which Version?",
-                        "There was a bug in v2.1.4 that lost teams before that. Would you like to restore your teams from before, or keep the new ones you made in 2.1.4?",
-                        "Use Old Teams",
-                        "Keep Teams from v2.1.4"
-                    );
+                    var title = Equals(TranslateExtension.Ci, CultureInfo.GetCultureInfo("fr"))
+                        ? "Quelle version ?"
+                        : "Which Version?";
+
+                    var message = Equals(TranslateExtension.Ci, CultureInfo.GetCultureInfo("fr"))
+                        ? "La mise à jour 2.1.4 a pu provoqué la suppression de vos équipes. Souhaitez-vous les récupérer, ou utiliser celle que vous avez créé dans la version 2.1.4 ?"
+                        : "There was a bug in v2.1.4 that lost teams before that. Would you like to restore your teams from before, or keep the new ones you made in 2.1.4?";
+
+                    var accept = Equals(TranslateExtension.Ci, CultureInfo.GetCultureInfo("fr"))
+                        ? "Récupérer mes anciennes équipes"
+                        : "Use Old Teams";
+
+                    var cancel = Equals(TranslateExtension.Ci, CultureInfo.GetCultureInfo("fr"))
+                        ? "Garder mes équipes v2.1.4"
+                        : "Keep Teams from v2.1.4";
+
+                    bool result = await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
                     if (result)
                     {
                         // deleting the old user db before we start the DBUpdater will cause it to fall back to the LegacyDB

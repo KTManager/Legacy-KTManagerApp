2c66e632d8fb805c2b911b914ddac46b9144675a
Jake Cordero <jakemcordero@gmail.com>
2020-03-22 06:50:38 -0700

    Use a list view instead

--- KillTeam/ViewModels/VersionPageViewModel.cs
+++ KillTeam/ViewModels/VersionPageViewModel.cs
@@ -1,22 +1,35 @@
 ﻿using KillTeam.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 using Xamarin.Essentials;
 
 namespace KillTeam.ViewModels
 {
+
+    class VersionInfo
+    {
+        public string Title;
+        public string Detail;
+    }
     class VersionPageViewModel
     {
-        public VersionPageViewModel() { }
+        private ObservableCollection<VersionInfo> versionList;
+        public ObservableCollection<VersionInfo> VersionList => versionList;
+        public VersionPageViewModel()
+        {
+            var userDB = KTContext.Db as KTUserContext;
 
-        public string AppVersion {
-            get => VersionTracking.CurrentVersion;
-        }
+            versionList.Add(new VersionInfo { Title = "App Version", Detail = VersionTracking.CurrentVersion });
+            versionList.Add(new VersionInfo { Title = "Rules Version", Detail = userDB.GetCurrentVersion().RulesVersion });
 
-        public string RulesVersion {
-            get => (KTContext.Db as KTUserContext).GetCurrentVersion().RulesVersion;
+            var importVersion = userDB.GetCurrentVersion().AppVersion;
+            if (importVersion != null)
+            {
+                versionList.Add(new VersionInfo { Title = "Last Rules Import App Version", Detail = importVersion });
+            }
         }
     }
 }
